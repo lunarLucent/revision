021eb1fc385f038f3382c3bff64e0597cf544257
lunarLucent <109795653+lunarLucent@users.noreply.github.com>
2023-05-22 09:24:08 +0100

    Add files via upload

---
+++ Petes Plumbers/clients/clients/clients/clients.Designer.vb
@@ -0,0 +1,212 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class clients
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtTitle = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtClientID = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.cmdSave = New System.Windows.Forms.Button()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.txtSurname = New System.Windows.Forms.TextBox()
+        Me.txtFirstname = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cmdCount = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtTitle
+        '
+        Me.txtTitle.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.txtTitle.Location = New System.Drawing.Point(98, 64)
+        Me.txtTitle.Name = "txtTitle"
+        Me.txtTitle.Size = New System.Drawing.Size(112, 20)
+        Me.txtTitle.TabIndex = 2
+        Me.txtTitle.Text = "Dr"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(8, 136)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(45, 13)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "Address"
+        '
+        'txtClientID
+        '
+        Me.txtClientID.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.txtClientID.Location = New System.Drawing.Point(98, 40)
+        Me.txtClientID.Name = "txtClientID"
+        Me.txtClientID.Size = New System.Drawing.Size(112, 20)
+        Me.txtClientID.TabIndex = 1
+        Me.txtClientID.Text = "1"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(8, 112)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(49, 13)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Surname"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(8, 88)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 13)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Firstname"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(8, 64)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(27, 13)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Title"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.Panel1.Controls.Add(Me.cmdSave)
+        Me.Panel1.Controls.Add(Me.lblName)
+        Me.Panel1.Controls.Add(Me.txtAddress)
+        Me.Panel1.Controls.Add(Me.txtSurname)
+        Me.Panel1.Controls.Add(Me.txtFirstname)
+        Me.Panel1.Controls.Add(Me.txtTitle)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.txtClientID)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.cmdCount)
+        Me.Panel1.Location = New System.Drawing.Point(-6, -6)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(305, 284)
+        Me.Panel1.TabIndex = 22
+        '
+        'cmdSave
+        '
+        Me.cmdSave.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.cmdSave.Location = New System.Drawing.Point(32, 234)
+        Me.cmdSave.Name = "cmdSave"
+        Me.cmdSave.Size = New System.Drawing.Size(96, 32)
+        Me.cmdSave.TabIndex = 18
+        Me.cmdSave.Text = "Save"
+        Me.cmdSave.UseVisualStyleBackColor = False
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.lblName.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(6, 6)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(87, 29)
+        Me.lblName.TabIndex = 9
+        Me.lblName.Text = "Clients"
+        '
+        'txtAddress
+        '
+        Me.txtAddress.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.txtAddress.Location = New System.Drawing.Point(98, 136)
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(112, 20)
+        Me.txtAddress.TabIndex = 5
+        Me.txtAddress.Text = "245 Western Avenue"
+        '
+        'txtSurname
+        '
+        Me.txtSurname.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.txtSurname.Location = New System.Drawing.Point(98, 112)
+        Me.txtSurname.Name = "txtSurname"
+        Me.txtSurname.Size = New System.Drawing.Size(112, 20)
+        Me.txtSurname.TabIndex = 4
+        Me.txtSurname.Text = "Other"
+        '
+        'txtFirstname
+        '
+        Me.txtFirstname.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.txtFirstname.Location = New System.Drawing.Point(98, 88)
+        Me.txtFirstname.Name = "txtFirstname"
+        Me.txtFirstname.Size = New System.Drawing.Size(112, 20)
+        Me.txtFirstname.TabIndex = 3
+        Me.txtFirstname.Text = "Ann"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(8, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "ClientID"
+        '
+        'cmdCount
+        '
+        Me.cmdCount.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.cmdCount.Location = New System.Drawing.Point(152, 234)
+        Me.cmdCount.Name = "cmdCount"
+        Me.cmdCount.Size = New System.Drawing.Size(96, 32)
+        Me.cmdCount.TabIndex = 0
+        Me.cmdCount.Text = "Count"
+        Me.cmdCount.UseVisualStyleBackColor = False
+        '
+        'clients
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(292, 273)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "clients"
+        Me.Text = "Clients"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents txtTitle As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtClientID As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Private WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents cmdSave As System.Windows.Forms.Button
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents txtAddress As System.Windows.Forms.TextBox
+    Friend WithEvents txtSurname As System.Windows.Forms.TextBox
+    Friend WithEvents txtFirstname As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents cmdCount As System.Windows.Forms.Button
+
+End Class
---
+++ Petes Plumbers/clients/clients/clients/clients.vb
@@ -0,0 +1,71 @@
+﻿Imports System.IO
+
+Public Class clients
+
+
+    '  . . . means missing or incomplete code
+
+
+
+    Private Structure client
+        Public ClientID As String
+        Public Title As String
+        Public Firstname As String
+        Public Surname As String
+        Public Address As String
+    End Structure
+
+    Private Sub Clientss_Load() Handles MyBase.Load
+        If Dir$("clients.txt") = "" Then
+            Dim sw As New StreamWriter("clients.txt", True)
+            sw.WriteLine("")
+            sw.Close()
+            MsgBox("A new file has been created", vbExclamation, "Warning!")
+        End If
+    End Sub
+
+    Private Sub cmdSave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdSave.Click
+        Dim clientData As New client
+        Dim sw As New System.IO.StreamWriter("clients.txt", True)
+        clientData.ClientID = LSet(txtClientID.Text, 50)
+        clientData.Title = LSet(txtTitle.Text, 50)
+        clientData.Firstname = LSet(txtFirstname.Text, 50)
+        clientData.Surname = LSet(txtSurname.Text, 50)
+        clientData.Address = LSet(txtAddress.Text, 50)
+
+        sw.WriteLine(clientData.ClientID & clientData.Title & clientData.Firstname & clientData.Surname & clientData.Address)
+        sw.Close()
+        MsgBox("File Saved!")
+    End Sub
+
+    Private Sub cmdCount_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdCount.Click
+        Dim CountGot As Integer
+        CountGot = 0
+        Dim clientCount As Integer
+        clientCount = 0
+
+        Dim clientData() As String = File. '  . . . (" '  . . . .txt")
+        For i = 0 To UBound(clientData)
+
+            CountGot = 0
+            If Trim(Mid(clientData(i), 1, 50)) = txtClientID.Text And Not txtClientID.Text = "" Then CountGot = CountGot + 1
+            If Trim(Mid(clientData(i), 51, 50)) =  '  . . . .Text And Not txtTitle.Text = "" Then CountGot = CountGot + 1
+            If Trim(Mid(clientData(i), 101, 50)) = . . . ..Text And Not txtFirstname.Text = "" Then CountGot = CountGot + 1
+            If Trim(Mid(clientData(i), 151, 50)) = . . . ..Text And Not txtSurname.Text = "" Then CountGot = CountGot + 1 'Counting how many attributes follow the search
+            If Trim(Mid(clientData(i), 201, 50)) = . . . ..Text And Not txtAddress.Text = "" Then CountGot = CountGot + 1
+                If CountGot > 0 Then  '  . . .  = clientCount + 1 
+        Next i
+        MsgBox("There were: " & clientCount & " Found")
+
+    End Sub
+
+
+
+
+
+
+
+
+
+
+End Class
